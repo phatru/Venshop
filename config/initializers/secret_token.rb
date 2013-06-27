ebb08edfc1805f3d89927bb78bcb63242c761a80
Hieu Phan <ptrhieu.it@gmail.com>
2013-06-27 21:59:46 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Venshop::Application.config.secret_key_base = '3f2cc0eb60a47ba31a40886d1a8bbceacf13ac55bdf8135ddcd1542ae4974e93aed5cbaa826f21467e24018c2c8d564737500eb9add1c2924753fd90b8a44c09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Venshop::Application.config.session_store :cookie_store, key: '_Venshop_session'
